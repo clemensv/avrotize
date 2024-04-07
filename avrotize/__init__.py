d76c216860f84d12ac98ffc6f6c8f5b0886f0569
Clemens Vasters <clemens@vasters.com>
2024-04-07 11:38:33 +0200

    package exports
    Signed-off-by: Clemens Vasters <clemens@vasters.com>

--- avrotize/__init__.py
+++ avrotize/__init__.py
@@ -1,6 +1,28 @@
 # __init__.py
 
-from .prototoavro import convert_proto_to_avro
+from .jsonstoavro import convert_jsons_to_avro
+from .kstructtoavro import convert_kafka_struct_to_avro_schema
+from .asn1toavro import convert_asn1_to_avro
+from .xsdtoavro import convert_xsd_to_avro
+from .prototoavro import  convert_proto_to_avro
+from .avrotojsons import convert_avro_to_json_schema
+from .avrotokusto import convert_avro_to_kusto
+from .avrotoparquet import convert_avro_to_parquet
+from .avrotoproto import convert_avro_to_proto
+from .avrototsql import convert_avro_to_tsql
+from .avrotoxsd import convert_avro_to_xsd
 
-
-
+__all__ = [
+    "convert_proto_to_avro",
+    "convert_jsons_to_avro",
+    "convert_kafka_struct_to_avro_schema",
+    "convert_asn1_to_avro",
+    "convert_xsd_to_avro",
+    "convert_proto_to_avro",
+    "convert_avro_to_json_schema",
+    "convert_avro_to_kusto",
+    "convert_avro_to_parquet",
+    "convert_avro_to_proto",
+    "convert_avro_to_tsql",
+    "convert_avro_to_xsd"
+]
